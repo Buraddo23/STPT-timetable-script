7eaa2c3f1887cf459f1633ef5db6c83684738940
Vlad Sîngeorzan <vlad.singeorzan@student.upt.ro>
2020-06-06 15:38:59 +0300

    Added support for 3 column timetables (separate saturday and sunday)

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-## TO DO:
+School lines are out of scope for the project.  
+Lines not mentioned here have been tested and appear to work correctly
 
-- E1, E3-3, E4, E4b, E6, M11, M14, M49 have separate columns for weekend, currently not implemented
+## TO DO:
 - Integration of scheduled update and notification of changes
 
 ## Bugs related to the current timetables (06.2020)
 - 29/M29, M30, M35, M42: last timeslot incorrect due to mispelling
-- E2: weekend time row is delayed, generates a lot of mistakes
-- E7: first timeslot ignored
+- E2, E4: weekend time row is delayed, generates a lot of mistakes
+- E4b, E7, M11, M14: Empty columns generates some bad timeslots
 
 ## Lines that need manual work:
 - **28**: due to combination with 21 in weekends
@@ -17,6 +18,7 @@
 - _**E2**_ (a): weekend route different
 - _**E3**_: due to grouping of E3 and 3 lines, we generate timetables only for the ends (i.e. start of 3, end of E3)
 - **E4** has a shitlot of variants, needs manual verification of individual pages
+- *M11* the first timeslot starts from the second station
 - **M22** also has a shitlod of variants, needs manual reverse engineering of bus routes and timetables
 - _**M35**_: one timeslot only during school (currently ignored by script). The line had a variant, due to this, the b side doesn't generate a timetable.
 - **M36, M41, M44, M46, M47, M48** have variants
@@ -26,8 +28,3 @@
 **Bold lines need complete manual creation of timetable**  
 _**Mixed lines produce correct data partially**_  
 *Italic lines need minimal intervention on output*
-
-## Additional information:
-
-School lines are out of scope for the project.  
-Lines not mentioned here have been tested and appear to work correctly
--- parser.pl
+++ parser.pl
@@ -1,56 +1,113 @@
 #!/usr/bin/env perl
 use strict;
-use warnings;
+#use warnings;
 use Data::Printer;
 
 my $input_file_name = shift or die "Usage: $0 INPUT_FILE\n";
 my $work_file_name;
 my $free_file_name;
+my $saturday_file_name;
+my $sunday_file_name;
 
 if ($input_file_name =~ /(.*\/|)(.*)\.txt/) {
-	$work_file_name = "output_txt_files/$2s.txt";
+	$work_file_name = "output_txt_files/$2l.txt";
 	$free_file_name = "output_txt_files/$2w.txt";
+	$saturday_file_name = "output_txt_files/$2s.txt";
+	$sunday_file_name = "output_txt_files/$2d.txt";	
+} else {
+	die "Invalid input file";
 }
 
 open(my $input, '<', $input_file_name) or die "Couldn't open input file $input_file_name";
 open(my $work_output, '>', $work_file_name) or die "Couldn't write in weekday file $work_file_name";
-open(my $free_output, '>', $free_file_name) or die "Couldn't write in weekend file $free_file_name";
+my $free_output;
+my $saturday_output;
+my $sunday_output;
+
+my $layout;
 
 while (<$input>) {
 	last if ($_ =~ //); #new page character (FF) (0x0C)
 	my @line_fields = split(/\s+/, $_);
 	shift @line_fields unless ($line_fields[0]);
-	my $is_weekday = 1;
 	
 	next if (scalar @line_fields < 3);
 	next if (scalar @line_fields == 3 && grep(/-/,@line_fields));
-	next if (grep(/[A-z]/,@line_fields));
-
-	my $hi = $#line_fields;
-	for my $i (1 .. $#line_fields-1) {
-		if ($line_fields[0] == $line_fields[$i]) {
-			$hi = $i;
-			last if ($line_fields[$i-1] > $line_fields[$i]);
-			last if ($line_fields[$i]   > $line_fields[$i+1]);
+	if (grep(/[A-z]/,@line_fields)) {
+		if (grep(/NELUCRĂTOARE/,@line_fields)) {
+			$layout = 2;
+			open($free_output, '>', $free_file_name) or die "Couldn't write in weekend file $free_file_name";
 		}
+		if (grep(/(SÂMBĂTĂ|SAMBATA)/,@line_fields)) {
+			$layout = 3;
+			open($saturday_output, '>', $saturday_file_name) or die "Couldn't write in weekend file $saturday_file_name";
+			open($sunday_output,   '>', $sunday_file_name)   or die "Couldn't write in weekend file $sunday_file_name";			
+		}
+		next;
 	}
-	
-	for my $i (1 .. $#line_fields) {	
-		my $time = $line_fields[0].':'.$line_fields[$i];
-		if ($time =~ /\d{1,2}:\d{1,2}/) {
-			if ($i == $hi) {
-				$is_weekday = 0;
-			} else {
-				if ($is_weekday) {
-					print $work_output "$time\n";
+
+	if ($layout == 2) {
+		my $is_weekday = 1;
+		
+		my $hi = $#line_fields;
+		for my $i (1 .. $#line_fields-1) {
+			if ($line_fields[0] == $line_fields[$i]) {
+				$hi = $i;
+				last if ($line_fields[$i-1] > $line_fields[$i]);
+				last if ($line_fields[$i]   > $line_fields[$i+1]);
+			}
+		}
+		
+		for my $i (1 .. $#line_fields) {	
+			my $time = $line_fields[0].':'.$line_fields[$i];
+			if ($time =~ /\d{1,2}:\d{1,2}/) {
+				if ($i == $hi) {
+					$is_weekday = 0;
 				} else {
-					print $free_output "$time\n";
+					if ($is_weekday) {
+						print $work_output "$time\n";
+					} else {
+						print $free_output "$time\n";
+					}
 				}
 			}
 		}
+	} elsif ($layout == 3) {
+		my $column = 1;
+		
+		my @hi;
+		for my $i (1 .. $#line_fields-1) {
+			push(@hi,$i) if ($line_fields[0] == $line_fields[$i] && $hi[-1] != $i-1);
+		}
+		
+		print "Incorrect layout on $input_file_name, hour $line_fields[0]\n" if (scalar @hi != 2);
+		
+		for my $i (1 .. $#line_fields) {	
+			my $time = $line_fields[0].':'.$line_fields[$i];
+			if ($time =~ /\d{1,2}:\d{1,2}/) {
+				if (grep(/$i/,@hi)) {
+					$column++;
+				} else {
+					if ($column == 1) {
+						print $work_output "$time\n";
+					} elsif ($column == 2) {
+						print $saturday_output "$time\n";
+					} else {
+						print $sunday_output "$time\n";
+					}
+				}
+			}
+		}
+	} else {
+		die "Unknown layout for file $input_file_name";
 	}
 }
 
 close $input;
 close $work_output;
-close $free_output;
+if ($layout == 2) {
+	close $free_output;
+} elsif ($layout == 3) {
+	close $saturday_output;
+	close $sunday_output;
+}
